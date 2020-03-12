c6856c2337ecfb963fe9ba6c0e16fcaeff37507e
code-mds <43860962+code-mds@users.noreply.github.com>
2020-03-12 15:26:29 +0100

    S5 refactoring

---
+++ s5_casa/draw_utils.cpp
@@ -0,0 +1,127 @@
+//
+// Created by massi on 05.03.2020.
+//
+#include "draw_utils.h"
+
+bool showAxis = true;
+bool showWireFrame = true;
+
+void internal_triangle3D(const vertex_t &v1, const vertex_t &v2, const vertex_t &v3);
+
+void draw_prism(triangle_t &front, triangle_t &back) {
+
+    rectangle_t rect[] = {
+            {
+                front.v2,
+                front.v1,
+                back.v2,
+                back.v1,
+                front.color
+            },
+            {
+                front.v3,
+                back.v3,
+                back.v2,
+                front.v1,
+                front.color
+            },
+            {
+                back.v3,
+                front.v3,
+                front.v2,
+                back.v1,
+                front.color
+            },
+    };
+
+    draw_rectangle3D(rect[0]);
+    draw_rectangle3D(rect[1]);
+    draw_rectangle3D(rect[2]);
+
+    draw_triangle3D(front);
+    draw_triangle3D(back);
+}
+
+void draw_parallelepiped(rectangle_t &front, rectangle_t &back) {
+    draw_rectangle3D(front);
+    draw_rectangle3D(back);
+
+    rectangle_t right = {front.v2, back.v1, back.v4, front.v3, front.color  };
+    draw_rectangle3D(right);
+
+    rectangle_t left = {back.v2, front.v1, front.v4, back.v3, front.color  };
+    draw_rectangle3D(left);
+
+    rectangle_t bottom = {back.v2, back.v1, front.v2, front.v1, front.color  };
+    draw_rectangle3D(bottom);
+
+    rectangle_t top = {front.v4, front.v3, back.v4, back.v3, front.color  };
+    draw_rectangle3D(top);
+}
+
+void draw_rectangle3D(rectangle_t &rect ) {
+    draw_triangle3D(rect.v1, rect.v2, rect.v3, rect.color);
+    draw_triangle3D(rect.v3, rect.v4, rect.v1, rect.color);
+}
+
+void draw_triangle3D(triangle_t &triangle) {
+    draw_triangle3D(triangle.v1, triangle.v2, triangle.v3, triangle.color);
+}
+
+void draw_triangle3D(vertex_t &v1, vertex_t &v2, vertex_t &v3, color_t &color) {
+    glColor3ub(color.r, color.g, color.b);
+    glPolygonMode(GL_FRONT,GL_FILL);
+    internal_triangle3D(v1, v2, v3);
+
+    if(showWireFrame) {
+        // wireframe
+        glLineWidth(1.0);
+        glColor3ub(15, 32, 112);
+        glPolygonMode(GL_FRONT, GL_LINE);
+        internal_triangle3D(v1, v2, v3);
+    }
+}
+
+void internal_triangle3D(const vertex_t &v1, const vertex_t &v2, const vertex_t &v3) {
+    glBegin(GL_TRIANGLES);
+    glVertex3f(v1.x, v1.y, v1.z);
+    glVertex3f(v2.x, v2.y, v2.z);
+    glVertex3f(v3.x, v3.y, v3.z);
+    glEnd();
+}
+
+
+void draw_axis() {
+    if(!showAxis)
+        return;
+
+    glLineWidth(1.0f);
+    glEnable(GL_LINE_STIPPLE);
+    glLineStipple(1, 0xf0f0);
+    // axes are ten units long.
+    glBegin(GL_LINES);
+    // Draw a red x-axis
+    glColor3ub(255, 0, 0);
+    glVertex3f(-2, 0, 0);
+    glVertex3f(2, 0, 0);
+    // Draw a green y-axis
+    glColor3ub(0, 255, 0);
+    glVertex3f(0, -2, 0);
+    glVertex3f(0, 2, 0);
+    // Draw a blue z-axis
+    glColor3ub(0, 0, 255);
+    glVertex3f(0, 0, -2);
+    glVertex3f(0, 0, 2);
+    glEnd();
+    glDisable(GL_LINE_STIPPLE);
+}
+
+void toggleAxesVisibility() {
+    showAxis = !showAxis;
+    glutPostRedisplay();
+}
+
+void toggleWireframeVisibility() {
+    showWireFrame = !showWireFrame;
+    glutPostRedisplay();
+}
---
+++ s5_casa/main.cpp
@@ -0,0 +1,278 @@
+//glut_test.cpp
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+#ifdef __APPLE__
+    // Headers richiesti da OSX
+    #include <GL/glew.h>
+    #include <GLUT/glut.h>
+#else
+    // headers richiesti da Windows e linux
+    #include <GL\glew.h>
+    #include <GL\freeglut.h>
+#endif
+#include "draw_utils.h"
+
+#define COLOR_ROOF_EXTERNAL     230, 0, 0
+#define COLOR_ROOF_INTERNAL     160, 0, 0
+#define COLOR_WALL_EXTERNAL     200, 200, 200
+#define COLOR_WALL_INTERNAL     80, 80, 80
+#define COLOR_FLOOR             160, 160, 160
+#define COLOR_BACK              1.0, 1.0, 1.0, 0.0
+
+const int WINDOW_WIDTH = 600;
+const int WINDOW_HEIGHT = 600;
+const float SW = 10.0f; //SCENE WIDTH
+
+const float HALF_DOOR_WIDTH = 2.0f;
+const float HALF_BASE_WIDTH = 5.0f;
+const float BASE_HEIGHT = 8.0f;
+const float WALL_WIDTH = 1.0f;
+const float WALL_HEIGHT = 6.0f;
+const float ROOF_HEIGHT = 11.0f;
+
+void draw_prism_walls();
+void draw_lateral_walls();
+void draw_roof();
+void draw_floor();
+
+void draw() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    draw_axis();
+    draw_floor();
+    draw_lateral_walls();
+    draw_prism_walls();
+    draw_roof();
+    glutSwapBuffers();
+}
+
+void draw_floor() {
+    rectangle_t rect = {
+            // floor
+            {HALF_BASE_WIDTH / SW, 0,  0},
+            {HALF_BASE_WIDTH / SW, 0, -BASE_HEIGHT / SW},
+            {-HALF_BASE_WIDTH / SW, 0, -BASE_HEIGHT / SW},
+            {-HALF_BASE_WIDTH / SW, 0, 0},
+            COLOR_FLOOR
+    };
+
+    draw_rectangle3D(rect);
+}
+
+void draw_roof() {
+    rectangle_t rectangles[] = {
+            // right roof wall
+            {
+                    { WALL_HEIGHT/SW, WALL_HEIGHT/SW, WALL_WIDTH/SW },
+                    { WALL_HEIGHT/SW,  WALL_HEIGHT/SW, -(BASE_HEIGHT+WALL_WIDTH)/SW},
+                    { 0,  (ROOF_HEIGHT+WALL_WIDTH)/SW, -(BASE_HEIGHT+WALL_WIDTH)/SW },
+                    { 0,  (ROOF_HEIGHT+WALL_WIDTH)/SW, WALL_WIDTH/SW },
+                    COLOR_ROOF_EXTERNAL
+            },{
+                    { WALL_HEIGHT/SW,  HALF_BASE_WIDTH/SW, -(BASE_HEIGHT+WALL_WIDTH)/SW},
+                    { WALL_HEIGHT/SW, HALF_BASE_WIDTH/SW, WALL_WIDTH/SW },
+                    { 0,  ROOF_HEIGHT/SW, WALL_WIDTH/SW },
+                    { 0,  ROOF_HEIGHT/SW, -(BASE_HEIGHT+WALL_WIDTH)/SW },
+                    COLOR_ROOF_INTERNAL
+            },
+            // left roof wall
+            {
+                    { -WALL_HEIGHT/SW,  WALL_HEIGHT/SW, -(BASE_HEIGHT+WALL_WIDTH)/SW},
+                    { -WALL_HEIGHT/SW, WALL_HEIGHT/SW, WALL_WIDTH/SW },
+                    { 0,  (ROOF_HEIGHT+WALL_WIDTH)/SW, WALL_WIDTH/SW },
+                    { 0,  (ROOF_HEIGHT+WALL_WIDTH)/SW, -(BASE_HEIGHT+WALL_WIDTH)/SW },
+                    COLOR_ROOF_EXTERNAL
+            },{
+                    { -WALL_HEIGHT/SW, HALF_BASE_WIDTH/SW, WALL_WIDTH/SW },
+                    { -WALL_HEIGHT/SW,  HALF_BASE_WIDTH/SW, -(BASE_HEIGHT+WALL_WIDTH)/SW},
+                    { 0,  ROOF_HEIGHT/SW, -(BASE_HEIGHT+WALL_WIDTH)/SW },
+                    { 0,  ROOF_HEIGHT/SW, WALL_WIDTH/SW },
+                    COLOR_ROOF_INTERNAL
+            }
+    };
+
+    draw_parallelepiped(rectangles[0], rectangles[1]);
+    draw_parallelepiped(rectangles[2], rectangles[3]);
+}
+
+void draw_prism_walls() {
+   triangle_t triangles[] = {
+            {
+                    { -HALF_BASE_WIDTH/SW,  WALL_HEIGHT/SW, 0 },
+                    { HALF_BASE_WIDTH/SW, WALL_HEIGHT/SW, 0 },
+                    { 0,  ROOF_HEIGHT/SW, 0 },
+                    COLOR_WALL_EXTERNAL
+            },
+            {
+                    { HALF_BASE_WIDTH/SW, WALL_HEIGHT/SW, -WALL_WIDTH/SW },
+                    { -HALF_BASE_WIDTH/SW,  WALL_HEIGHT/SW,  -WALL_WIDTH/SW},
+                    { 0,  ROOF_HEIGHT/SW, -WALL_WIDTH/SW },
+                    COLOR_WALL_INTERNAL
+            },
+            {
+                    { HALF_BASE_WIDTH/SW, WALL_HEIGHT/SW, -(BASE_HEIGHT)/SW },
+                    { -HALF_BASE_WIDTH/SW,  WALL_HEIGHT/SW, -(BASE_HEIGHT)/SW},
+                    { 0,  ROOF_HEIGHT/SW, -BASE_HEIGHT/SW },
+                    COLOR_WALL_EXTERNAL
+            },
+            {
+                    { -HALF_BASE_WIDTH/SW,  WALL_HEIGHT/SW, -(BASE_HEIGHT-WALL_WIDTH)/SW},
+                    { HALF_BASE_WIDTH/SW, WALL_HEIGHT/SW, -(BASE_HEIGHT-WALL_WIDTH)/SW },
+                    { 0,  ROOF_HEIGHT/SW, -(BASE_HEIGHT-WALL_WIDTH)/SW },
+                    COLOR_WALL_INTERNAL
+            },
+    };
+
+    draw_prism(triangles[0], triangles[1]);
+    draw_prism(triangles[2], triangles[3]);
+}
+
+void draw_lateral_walls() {
+
+    rectangle_t rectangles[10] = {
+            // front walls
+            {
+                    { HALF_DOOR_WIDTH/SW, 0, 0 },
+                    { HALF_BASE_WIDTH/SW, 0, 0 },
+                    { HALF_BASE_WIDTH/SW, WALL_HEIGHT/SW, 0 },
+                    { HALF_DOOR_WIDTH/SW, WALL_HEIGHT/SW, 0 },
+                    COLOR_WALL_EXTERNAL
+            },
+            {
+                    { (HALF_BASE_WIDTH-WALL_WIDTH)/SW, 0, -WALL_WIDTH/SW },
+                    { HALF_DOOR_WIDTH/SW, 0, -WALL_WIDTH/SW },
+                    { HALF_DOOR_WIDTH/SW, WALL_HEIGHT/SW, -WALL_WIDTH/SW },
+                    { (HALF_BASE_WIDTH-WALL_WIDTH)/SW, WALL_HEIGHT/SW, -WALL_WIDTH/SW },
+                    COLOR_WALL_INTERNAL
+            },
+            {
+                    { -HALF_BASE_WIDTH/SW, 0, 0},
+                    { -HALF_DOOR_WIDTH/SW, 0, 0 },
+                    { -HALF_DOOR_WIDTH/SW, WALL_HEIGHT/SW, 0 },
+                    { -HALF_BASE_WIDTH/SW, WALL_HEIGHT/SW, 0 },
+                    COLOR_WALL_EXTERNAL
+            },
+            {
+                    { -HALF_DOOR_WIDTH/SW, 0, -WALL_WIDTH/SW },
+                    { -(HALF_BASE_WIDTH-WALL_WIDTH)/SW, 0, -WALL_WIDTH/SW},
+                    { -(HALF_BASE_WIDTH-WALL_WIDTH)/SW, WALL_HEIGHT/SW, -WALL_WIDTH/SW },
+                    { -HALF_DOOR_WIDTH/SW, WALL_HEIGHT/SW, -WALL_WIDTH/SW },
+                    COLOR_WALL_INTERNAL
+            },
+            //right wall
+            {
+                    { HALF_BASE_WIDTH/SW, 0, 0},
+                    { HALF_BASE_WIDTH/SW, 0, -BASE_HEIGHT/SW },
+                    { HALF_BASE_WIDTH/SW, WALL_HEIGHT/SW, -BASE_HEIGHT/SW },
+                    { HALF_BASE_WIDTH/SW, WALL_HEIGHT/SW, 0 },
+                    COLOR_WALL_EXTERNAL
+            },
+            {
+                    { (HALF_BASE_WIDTH-WALL_WIDTH)/SW, 0, -(BASE_HEIGHT-WALL_WIDTH)/SW },
+                    { (HALF_BASE_WIDTH-WALL_WIDTH)/SW, 0, -WALL_WIDTH/SW},
+                    { (HALF_BASE_WIDTH-WALL_WIDTH)/SW, WALL_HEIGHT/SW, -WALL_WIDTH/SW },
+                    { (HALF_BASE_WIDTH-WALL_WIDTH)/SW, WALL_HEIGHT/SW, -(BASE_HEIGHT-WALL_WIDTH)/SW },
+                    COLOR_WALL_INTERNAL
+            },
+            // back wall
+            {
+                    { HALF_BASE_WIDTH/SW, 0, -BASE_HEIGHT/SW },
+                    { -HALF_BASE_WIDTH/SW,  0, -BASE_HEIGHT/SW},
+                    { -HALF_BASE_WIDTH/SW,  WALL_HEIGHT/SW, -BASE_HEIGHT/SW },
+                    { HALF_BASE_WIDTH/SW,  WALL_HEIGHT/SW, -BASE_HEIGHT/SW },
+                    COLOR_WALL_EXTERNAL
+            },
+            {
+                    { -(HALF_BASE_WIDTH-WALL_WIDTH)/SW,  0, -(BASE_HEIGHT-WALL_WIDTH)/SW},
+                    { (HALF_BASE_WIDTH-WALL_WIDTH)/SW, 0, -(BASE_HEIGHT-WALL_WIDTH)/SW },
+                    { (HALF_BASE_WIDTH-WALL_WIDTH)/SW,  WALL_HEIGHT/SW, -(BASE_HEIGHT-WALL_WIDTH)/SW },
+                    { -(HALF_BASE_WIDTH-WALL_WIDTH)/SW,  WALL_HEIGHT/SW, -(BASE_HEIGHT-WALL_WIDTH)/SW },
+                    COLOR_WALL_INTERNAL
+            },
+            //left wall
+            {
+                    { -HALF_BASE_WIDTH/SW,  0, -BASE_HEIGHT/SW},
+                    { -HALF_BASE_WIDTH/SW, 0, 0 },
+                    { -HALF_BASE_WIDTH/SW,  WALL_HEIGHT/SW, 0 },
+                    { -HALF_BASE_WIDTH/SW,  WALL_HEIGHT/SW, -BASE_HEIGHT/SW },
+                    COLOR_WALL_EXTERNAL
+            },
+            {
+                    { -(HALF_BASE_WIDTH-WALL_WIDTH)/SW, 0, -WALL_WIDTH/SW },
+                    { -(HALF_BASE_WIDTH-WALL_WIDTH)/SW,  0, -(BASE_HEIGHT-WALL_WIDTH)/SW},
+                    { -(HALF_BASE_WIDTH-WALL_WIDTH)/SW,  WALL_HEIGHT/SW, -(BASE_HEIGHT-WALL_WIDTH)/SW },
+                    { -(HALF_BASE_WIDTH-WALL_WIDTH)/SW,  WALL_HEIGHT/SW, -WALL_WIDTH/SW },
+                    COLOR_WALL_INTERNAL
+            },
+    };
+    int nrOfWalls = sizeof(rectangles) / sizeof(rectangles[0]);
+    for (int i = 0; i < nrOfWalls; i=i+2) {
+        draw_parallelepiped(rectangles[i], rectangles[i + 1]);
+    }
+
+}
+
+void main_menu(int value)
+{
+    switch (value) {
+        case 1: toggleAxesVisibility(); break;
+        case 2: toggleWireframeVisibility(); break;
+    }
+}
+
+void init(void) {
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(COLOR_BACK);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -.5, 1.5, -1.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Position camera at (x1, y1, z1) looking at (x2, y2, y2) with the vector <0, 1, 0> pointing upward.
+    gluLookAt(-.6, 0.4, 1, 0, 0, 0, 0, 1, 0);
+
+    glutCreateMenu(main_menu);
+    glutAddMenuEntry("Show/Hide Axes", 1);
+    glutAddMenuEntry("Show/Hide Wireframe", 2);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+void keyboardS(int key, int x, int y) {
+    switch (key) {
+        case GLUT_KEY_F1:
+            toggleAxesVisibility();
+            break;
+        case GLUT_KEY_F2:
+            toggleWireframeVisibility();
+            break;
+        case GLUT_KEY_UP:
+            glRotatef(1.0,1.0,0.0,0.0);
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_DOWN:
+            glRotatef(1.0,-1.0,0.0,0.0);
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_RIGHT:
+            glRotatef(1.0,0.0,1.0,0.0);
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_LEFT:
+            glRotatef(-1.0,0.0,1.0,0.0);
+            glutPostRedisplay();
+            break;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("S5 house");
+    init();
+    glutDisplayFunc(draw);
+    glutSpecialFunc(keyboardS);
+    glutMainLoop();
+}
+
